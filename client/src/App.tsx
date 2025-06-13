fedd0d2cd61ffd72744df1ad36ec89d3527ca27c
Joseph White <jpwhite4@buffalo.edu>
2025-06-12 20:50:59 -0400

    Background colour

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,9 +27,7 @@ export default function App(props: { disableCustomTheme?: boolean }) {
           component="main"
           sx={(theme) => ({
             flexGrow: 1,
-            backgroundColor: theme.vars
-              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-              : alpha(theme.palette.background.default, 1),
+            background: 'linear-gradient(#B5EEFF, #31A7EB)',
             overflow: 'auto',
           })}
         >
